c9d8bb0a1a56b0b340d1535efc23c21a79c1a9db
Gaoxusheng <moxiaojulove@foxmail.com>
2024-07-13 23:47:10 +0800

    Simplify code

--- Forge/src/main/java/de/markusbordihn/easynpcepicfight/client/renderer/ClientRenderer.java
+++ Forge/src/main/java/de/markusbordihn/easynpcepicfight/client/renderer/ClientRenderer.java
@@ -20,6 +20,7 @@
 package de.markusbordihn.easynpcepicfight.client.renderer;
 
 import de.markusbordihn.easynpc.client.renderer.entity.layers.CustomHumanoidArmorLayer;
+import de.markusbordihn.easynpc.client.renderer.entity.standard.HumanoidModelRenderer;
 import de.markusbordihn.easynpc.client.renderer.entity.standard.HumanoidSlimModelRenderer;
 import de.markusbordihn.easynpc.client.renderer.entity.standard.IllagerModelRenderer;
 import de.markusbordihn.easynpc.client.renderer.entity.standard.IronGolemModelRenderer;
@@ -28,54 +29,51 @@ import de.markusbordihn.easynpc.client.renderer.entity.standard.ZombieModelRende
 import de.markusbordihn.easynpc.client.renderer.entity.standard.ZombieVillagerModelRenderer;
 import de.markusbordihn.easynpcepicfight.Constants;
 import de.markusbordihn.easynpcepicfight.entity.ModEntityType;
+import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import yesman.epicfight.client.events.engine.RenderEngine;
 
 @OnlyIn(Dist.CLIENT)
 public class ClientRenderer {
-    private static RenderEngine renderEngine = new RenderEngine();
-    protected static final Logger log = LogManager.getLogger(Constants.LOG_NAME);
 
-    private ClientRenderer() {
-    }
+  protected static final Logger log = LogManager.getLogger(Constants.LOG_NAME);
 
-    public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        log.info("{} Entity Renders ...", Constants.LOG_REGISTER_PREFIX);
-        event.registerEntityRenderer(ModEntityType.EVOKER.get(), IllagerModelRenderer::new);
-        event.registerEntityRenderer(
-                ModEntityType.HUMANOID_SLIM.get(),
-                context -> new HumanoidSlimModelRenderer(context, CustomHumanoidArmorLayer.class));
-        event.registerEntityRenderer(
-                ModEntityType.HUSK.get(),
-                context -> new ZombieModelRenderer(context, CustomHumanoidArmorLayer.class));
-        event.registerEntityRenderer(ModEntityType.ILLUSIONER.get(), IllagerModelRenderer::new);
-        event.registerEntityRenderer(ModEntityType.IRON_GOLEM.get(), IronGolemModelRenderer::new);
-        event.registerEntityRenderer(
-                ModEntityType.SKELETON.get(),
-                context -> new SkeletonModelRenderer(context, CustomHumanoidArmorLayer.class));
-        event.registerEntityRenderer(
-                ModEntityType.STRAY.get(),
-                context -> new SkeletonModelRenderer(context, CustomHumanoidArmorLayer.class));
-        event.registerEntityRenderer(ModEntityType.PILLAGER.get(), IllagerModelRenderer::new);
-        event.registerEntityRenderer(ModEntityType.VINDICATOR.get(), IllagerModelRenderer::new);
-        event.registerEntityRenderer(
-                ModEntityType.WITHER_SKELETON.get(),
-                context -> new SkeletonModelRenderer(context, CustomHumanoidArmorLayer.class));
-        event.registerEntityRenderer(
-                ModEntityType.ZOMBIE_VILLAGER.get(),
-                context -> new ZombieVillagerModelRenderer(context, CustomHumanoidArmorLayer.class));
-        event.registerEntityRenderer(
-                ModEntityType.ZOMBIE.get(),
-                context -> new HumanoidModelRenderer(context, PlayerArmorModelRenderer.class));
-        event.registerEntityRenderer(
-                ModEntityType.HUMANOID.get(),
-                context -> new HumanoidModelRenderer(context, PlayerArmorModelRenderer.class));
-        renderEngine.registerCustomEntityRenderer(ModEntityType.HUMANOID.get(), "player");
-        renderEngine.registerCustomEntityRenderer(ModEntityType.ZOMBIE.get(), "player");
-    }
+  private ClientRenderer() {}
 
+  public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+    log.info("{} Entity Renders ...", Constants.LOG_REGISTER_PREFIX);
+
+    event.registerEntityRenderer(ModEntityType.EVOKER.get(), IllagerModelRenderer::new);
+    event.registerEntityRenderer(
+        ModEntityType.HUMANOID.get(),
+        context -> new HumanoidModelRenderer(context, HumanoidArmorLayer.class));
+    event.registerEntityRenderer(
+        ModEntityType.HUMANOID_SLIM.get(),
+        context -> new HumanoidSlimModelRenderer(context, HumanoidArmorLayer.class));
+    event.registerEntityRenderer(
+        ModEntityType.HUSK.get(),
+        context -> new ZombieModelRenderer(context, HumanoidArmorLayer.class));
+    event.registerEntityRenderer(ModEntityType.ILLUSIONER.get(), IllagerModelRenderer::new);
+    event.registerEntityRenderer(ModEntityType.IRON_GOLEM.get(), IronGolemModelRenderer::new);
+    event.registerEntityRenderer(
+        ModEntityType.SKELETON.get(),
+        context -> new SkeletonModelRenderer(context, HumanoidArmorLayer.class));
+    event.registerEntityRenderer(
+        ModEntityType.STRAY.get(),
+        context -> new SkeletonModelRenderer(context, HumanoidArmorLayer.class));
+    event.registerEntityRenderer(ModEntityType.PILLAGER.get(), IllagerModelRenderer::new);
+    event.registerEntityRenderer(ModEntityType.VINDICATOR.get(), IllagerModelRenderer::new);
+    event.registerEntityRenderer(
+        ModEntityType.WITHER_SKELETON.get(),
+        context -> new SkeletonModelRenderer(context, HumanoidArmorLayer.class));
+    event.registerEntityRenderer(
+        ModEntityType.ZOMBIE.get(),
+        context -> new HumanoidModelRenderer(context, HumanoidArmorLayer.class));
+    event.registerEntityRenderer(
+        ModEntityType.ZOMBIE_VILLAGER.get(),
+        context -> new ZombieVillagerModelRenderer(context, HumanoidArmorLayer.class));
+  }
 }
